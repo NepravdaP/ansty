d37c2a13cc6d9019d2d74a51a0e3e513e58770a6
Pavel Harmaza <pavelharmaza@gmail.com>
2022-05-10 17:15:57 +0300

    feat: add desktop version

---
+++ src/components/Birthday/styled.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+export const BirthdayWrapper = styled.div`
+  min-height: 100vh;
+`;
+export const FooterPic = styled.img`
+  height: 100px;
+`;
+export const DropName = styled.div`
+  height: 20px;
+  width: 100px;
+  background-color: #5f4764;
+  border-radius: 10px;
+  position: absolute;
+  color: #fff;
+  text-align: center;
+`;
+export const PicWrap = styled.div`
+  margin: 40px 40px;
+`;
+export const Balloon = styled.div`
+  height: 125px;
+  width: 105px;
+  border-radius: 75% 75% 70% 70%;
+  position: relative;
+`;
+export const BalloonWrapper = styled.div`
+  position: absolute;
+  /* transform: translate(-50%, -50%); */
+  height: 100vh;
+  overflow: hidden;
+  padding: 1em;
+  box-sizing: border-box;
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  z-index: 10000;
+`;
